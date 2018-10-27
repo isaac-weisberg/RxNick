ac82a86c7df577166f28fdf91213cfd6f553f547
Isaac Carol Weisberg <isaac_weisberg@outlook.com>
2018-10-27 16:22:34 +0700

    Add a get and post request

--- RxNick/RxNick.swift
+++ RxNick/RxNick.swift
@@ -9,32 +9,62 @@
 import Foundation
 import RxSwift
 
-public class Response {
-    public let res: HTTPURLResponse
-    public let data: Data?
-    
-    init(res: HTTPURLResponse, data: Data?) {
-        self.res = res
-        self.data = data
+public extension RxNick {
+    enum Method: String {
+        case GET
+        case POST
     }
 }
 
-func request(_ req: URLRequest, session: URLSession = URLSession.shared) -> Single<Response> {
-    return Single.create { single in
-        var task: URLSessionDataTask? = session.dataTask(with: req) { data, response, error in
-            if let error = error {
-                single(.error(error))
-                return
+public extension RxNick {
+    public class Response {
+        public let res: HTTPURLResponse
+        public let data: Data?
+        
+        init(res: HTTPURLResponse, data: Data?) {
+            self.res = res
+            self.data = data
+        }
+    }
+}
+
+
+public class RxNick {
+    let session: URLSession
+    
+    init(_ session: URLSession = URLSession.shared) {
+        self.session = session
+    }
+    
+    func request(_ request: URLRequest) -> Single<Response> {
+        return Single.create {[session = session] single in
+            var task: URLSessionDataTask? = session.dataTask(with: request) { data, response, error in
+                if let error = error {
+                    single(.error(error))
+                    return
+                }
+                
+                let response = response as! HTTPURLResponse
+                let resp = Response(res: response, data: data)
+                single(.success(resp))
             }
             
-            let response = response as! HTTPURLResponse
-            let resp = Response(res: response, data: data)
-            single(.success(resp))
+            return Disposables.create {
+                task?.cancel()
+                task = nil
+            }
         }
-        
-        return Disposables.create {
-            task?.cancel()
-            task = nil
-        }
+    }
+    
+    func get(_ url: URL) -> Single<Response> {
+        let req = URLRequest(url: url)
+        return request(req)
+    }
+    
+    func post(_ url: URL, data: Data?) -> Single<Response> {
+        var req = URLRequest(url: url)
+        req.httpMethod = Method.POST.rawValue
+        req.httpBody = data
+        return request(req)
     }
 }
